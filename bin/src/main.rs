52572a10b1af66517c5ecb8593d797e7dabfca91
kivimango <balazsviktor23@gmail.com>
2025-04-23 19:40:11 +0200

    Display cpu info just for fun

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let cpu_info = core::get_cpu_info();
+
+    println!("🧠 CPU Info:");
+    println!("  Name: {}", cpu_info.name);
+    println!("  Cores: {}", cpu_info.core_count);
+    println!("  Avg Frequency: {} MHz", cpu_info.frequency);
+
+    if let Some(temp) = cpu_info.temperature {
+        println!("  Temperature: {:.1}°C", temp);
+    } else {
+        println!("  Temperature: N/A");
+    }
 }
