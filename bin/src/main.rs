3deb6127aa4c54c6e59857fcd0d33ae7ced544f7
kivimango <balazsviktor23@gmail.com>
2025-04-24 19:56:24 +0200

    Use View in main()

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,14 +1,6 @@
+use tui::View;
+
 fn main() {
-    let cpu_info = core::get_cpu_info();
-
-    println!("🧠 CPU Info:");
-    println!("  Name: {}", cpu_info.name);
-    println!("  Cores: {}", cpu_info.core_count);
-    println!("  Avg Frequency: {} MHz", cpu_info.frequency);
-
-    if let Some(temp) = cpu_info.temperature {
-        println!("  Temperature: {:.1}°C", temp);
-    } else {
-        println!("  Temperature: N/A");
-    }
+    let mut view = View::default();
+    view.run();
 }
