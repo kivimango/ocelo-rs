dd0b185bb4919f7fc594f73c14904d9440edca8a
kivimango <balazsviktor23@gmail.com>
2025-04-28 11:04:00 +0200

    Added SystemOverviewInfo

--- core/src/model/mod.rs
+++ core/src/model/mod.rs
@@ -3,3 +3,10 @@ mod system;
 
 pub use cpu::*;
 pub use system::*;
+
+/// Collection of system information to be displayed in the Overview component.
+#[derive(Debug, Clone)]
+pub struct SystemOverviewInfo {
+    pub cpu: CpuInfo,
+    pub overview: SystemInfo,
+}
