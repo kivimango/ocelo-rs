51e15ba818491468a3aef10a75f963413e63c4ec
kivimango <balazsviktor23@gmail.com>
2025-04-28 11:46:19 +0200

    Added from_json() for SystemOverviewInfo

--- core/src/model/mod.rs
+++ core/src/model/mod.rs
@@ -12,3 +12,10 @@ pub struct SystemOverviewInfo {
     pub cpu: CpuInfo,
     pub overview: SystemInfo,
 }
+
+impl SystemOverviewInfo {
+    /// Creates `self` from a JSON reprentation
+    pub fn from_json(value: &String) -> Result<Self, serde_json::Error> {
+        serde_json::from_str(&value)
+    }
+}
