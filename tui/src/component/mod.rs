ab9347d498cdc3f18fa37bcc7952d2955fbc1743
kivimango <balazsviktor23@gmail.com>
2025-05-03 01:58:12 +0200

    Added get_color_for() function

--- tui/src/component/mod.rs
+++ tui/src/component/mod.rs
@@ -1,3 +1,14 @@
-mod overview;
+use ratatui::style::{Style, Stylize};
 
+mod overview;
 pub use self::overview::*;
+
+pub fn get_color_for(percentage: f64) -> Style {
+    match percentage {
+        0.0..24.99 => Style::default().light_green(),
+        25.0..49.99 => Style::default().green(),
+        50.0..74.99 => Style::default().yellow(),
+        75.0..100.0 => Style::default().light_red(),
+        _ => Style::reset(),
+    }
+}
